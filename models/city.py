4f567ca8de959e06fc32e37a550223974ed0a5cf
Joseph Mariga <jsphmariga@gmail.com>
2022-09-14 22:32:13 +0000

    Update city and state

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlachemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name
+
+    Attributes:
+        __tablename__(str): cities table in MySQLdb
+        state_id(String): the state id
+        name(String): the state name
+    """
+    __tablename__ = 'cities'
+    state_id = Column(String(60), nullable=False, ForeignKey('states.id'))
+    name = Column(String(128), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class
+    Attributes:
+        __tablename__(str): states table in MySQLdb
+        name(String): states' name
+        cities(): relationship between cities and states table
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
