55f127e24ced317bf25a1e6c7a42545055cfda11
Joseph Mariga <jsphmariga@gmail.com>
2022-09-17 21:48:04 +0000

    Fix

--- models/state.py
+++ models/state.py
@@ -2,8 +2,11 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -16,3 +19,15 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """
+            Lists all related cit objects
+            """
+            cities = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
