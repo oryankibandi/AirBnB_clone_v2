80b909db1853855fdf62e66fad6f17a893e5b559
Joseph Mariga <jsphmariga@gmail.com>
2022-09-17 22:30:52 +0000

    Add amenities setter

--- models/place.py
+++ models/place.py
@@ -3,8 +3,10 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from models.review import Review
+from models.amenity import Amenity
 from sqlalchemy import Column, String, ForeignKey, Float, Integer
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Place(BaseModel, Base):
@@ -27,13 +29,13 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
+    description = Column(String(1024, nullable=True))
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
     _amenities = relationship('Amenity', secondary='place_amenity',
                               viewonly=False, back_populates="place_amenities")
@@ -48,3 +50,29 @@ class Place(BaseModel, Base):
             if value.place_id == self.id:
                 reviews.append(value)
         return reviews
+
+    @property
+    def amenities(self):
+        """
+        Returns list of amenities
+        """
+        return self._amenities
+
+    @amenities.setter
+    def amenities(self, value):
+        """
+        set amenities
+        """
+        if (getenv("HBNB_TYPE_STORAGE") != "db"):
+            try:
+                if (value.type(self).__name__ == "Amenity"):
+                    self.amenity_ids.append(value.id)
+            except Exception:
+                pass
+            from models import storage
+            all_amenities = storage.all(Amenity)
+            amen = []
+            for val in all_amenities.values():
+                if val.id in self.amenity_ids:
+                    amen.append(val)
+            self._amenities = amen
